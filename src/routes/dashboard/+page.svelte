ace7b9553c26afdd68f68d1b49175654d04d18c9
rustfan1987 <56850302+rustfan1987@users.noreply.github.com>
2023-04-23 01:21:15 -0700

    6

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -14,28 +14,50 @@
 		</a>
 	</div>
 
-	<div class="table-container mt-8">
-		<table class="table-hover table">
-			<thead>
-				<tr>
-					<th>Title</th>
-					<th>Created</th>
-					<th>Published</th>
-					<th>Actions</th>
-				</tr>
-			</thead>
+	<div class="">
+		<table class="">
 
-			<tbody>
+
+			<tbody><div class="box-content h-400 w-200 spellcard bg-gradient-to-r from-slate-200 to-slate-300 pt-5 shadow-md rounded-lg p-1 bg-cover bg-center border-4 border-slate-900">
 				{#each data.posts as post, i}
-					<tr>
+					
 						<td>
-							<a class="unstyled capitalize" href="/dashboard/edit/{post.slug}">
+							<div class="flex justify-around mb-1 mx-0 ">
+								<button class="bg-blue-500 hover:bg-blue-600 text-white font-medium w-6 h-6 rounded">1</button>
+								<button class="bg-blue-200 hover:bg-blue-600 text-white font-medium w-6 h-6 rounded">2</button>
+								<button class="bg-blue-300 hover:bg-blue-600 text-white font-medium w-6 h-6 rounded">3</button>
+								<button class="bg-blue-700 hover:bg-blue-600 text-white font-medium w-6 h-6 rounded">4</button>
+								<button class="bg-blue-800 hover:bg-blue-600 text-white font-medium w-6 h-6 rounded">5</button>
+							  </div>
+
+							  <div class=" flex justify-center ">
+								<div class="pr-2 flex flex-col justify-between ">
+								  <button class="bg-blue-500 hover:bg-blue-600 text-white font-medium w-6 h-6 rounded">A</button>
+								  <button class="bg-blue-600 hover:bg-blue-600 text-white font-medium w-6 h-6 rounded">B</button>
+								  <button class="bg-blue-700 hover:bg-blue-600 text-white font-medium w-6 h-6 rounded">C</button>
+								</div>
+								
+								<div class="pr-1.5 pl-1.5"><img src="./cheems.fw.png" alt="Favicon" style="width: 80px; height: 80px;"></div>
+								
+								<div class="pl-2.5 flex flex-col justify-between">
+								  <button class="bg-blue-700 hover:bg-blue-600 text-white font-medium w-6 h-6 rounded">$</button>
+								  <button class="bg-blue-800 hover:bg-blue-600 text-white font-medium w-6 h-6 rounded">!</button>
+								  <button class="bg-blue-900 hover:bg-blue-600 text-white font-medium w-6 h-6 rounded">?</button>
+								</div>
+							  </div>
+							<a class="pt-1 text-lg font-medium mt-1 text-center flex justify-center" href="/dashboard/edit/{post.slug}">
 								{truncate(post.title)}
 							</a>
-						</td>
-						<td>{date(post.createdAt)}</td>
-						<td>{post.published ? 'Yes' : 'No'}</td>
-						<td>
+
+							<div class="relative flex overflow-x-hidden"> 
+								<div class=" text-red-900 text-bold animate-marquee whitespace-nowrap">
+								  <span class="px-1">#Cheems</span>
+								  <span class="px-1">#Nice</span>
+								</div></div>
+
+						<div class="flex justify-center">{date(post.createdAt)}</div>
+						<div class="flex justify-center"></div>{post.published ? 'Yes' : 'No'}
+
 							<form
 								method="POST"
 								action="?/delete&slug={post.slug}"
@@ -45,10 +67,11 @@
 									Delete
 								</button>
 							</form>
-						</td>
-					</tr>
+
+					
 				{/each}
-			</tbody>
+			</div>
+		</tbody>
 		</table>
 	</div>
 </main>
